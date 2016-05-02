a55812aeb42a2542d03598227ba18ba828114b7a
Aoshi_F <blue_20will@yahoo.co.jp>
2016-05-02 14:13:25 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @messages = Message.all #Message全取得
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  
 end
