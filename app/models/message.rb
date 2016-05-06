90ef5928e5a015e3f731e34cb0a7e199282b7de6
Aoshi_F <blue_20will@yahoo.co.jp>
2016-05-06 13:52:09 +0000

    display age

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ActiveRecord::Base
   validates :name, length: { maximum: 20 }, presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body, length: { minimum: 2, maximum: 30 }, presence: true
-  # 年齢は0以上の正の整数のみ
-  validates :age, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  # 年齢は0以上の正の整数のみ 非必須入力
+  #validates :age, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates_numericality_of :age, greater_than_or_equal_to: 0, only_integer: true, allow_nil: true
 end
